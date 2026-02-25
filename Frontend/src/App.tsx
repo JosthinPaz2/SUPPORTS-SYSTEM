62254f70615095bf0b694b3ed922a9c437cd89be
Camilo Pinzon <camilo.pinzon@onetouchdirect.com>
2026-02-25 15:16:28 -0500

    Tablero Kanban

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -9,6 +9,7 @@ function App() {
     <AuthProvider>
       <TicketProvider>
         <BrowserRouter>
+        
           <Routes>
             <Route path="/login" element={<Login />} />
             <Route path="/admin" element={<AdminDashboard />} />
