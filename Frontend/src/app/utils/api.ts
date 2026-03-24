9a7f10f075890f2ed9dd1093141a436bb8c9dd1f
EstebanCedano17 <estebancedano173@gmail.com>
2026-03-24 08:41:33 -0500

    mapeo correcto pro max full hd 4k 100% real no fake

--- Frontend/src/app/utils/api.ts
+++ Frontend/src/app/utils/api.ts
@@ -1,7 +1,7 @@
 // API configuration and utilities
 const API_BASE_URL =
   (import.meta.env.VITE_API_BASE_URL as string | undefined)?.trim() ||
-  'https://margery-highfalutin-unambiguously.ngrok-free.dev';
+  'https://frizzier-rayna-crunchily.ngrok-free.dev ';
 
 export interface LoginRequest {
   institutional_email: string;
